aa5ef7f4f5296f9248b63bf869f693e01d94768f
Sho Hashimoto <hashimoto@shokai.org>
2010-11-02 10:50:17 +0900

    remove default of create_at on model

--- models.rb
+++ models.rb
@@ -3,13 +3,13 @@ require 'rubygems'
 class User
   include Mongoid::Document
   field :user_id
-  field :name, :default => ""
-  field :created_at, :type => Integer, :default => Time.now.to_i
+  field :name
+  field :created_at, :type => Integer
 end
 
 class Page
   include Mongoid::Document
   field :url
   field :user_id
-  field :created_at, :type => Integer, :default => Time.now.to_i
+  field :created_at, :type => Integer
 end
